bdde21ff79737bfe8c49087e43f2ac4c91f16527
dat-th23 <dat.th1230@gmail.com>
2024-04-10 11:25:35 +0700

    report month

---
+++ src/pages/pages/report-month/useUrlState.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
+
+export function useUrlState(initialState) {
+    const router = useRouter()
+    const [state, setState] = useState(initialState)
+
+    useEffect(() => {
+        const { pathname, query } = router
+        const newState = { ...initialState, ...query }
+        setState(newState)
+    }, [router])
+
+    const setUrlState = (newState) => {
+        const { pathname } = router
+        const url = {
+            pathname,
+            query: { ...router.query, ...newState }
+        }
+        router.push(url, undefined, { shallow: true })
+    }
+
+    const incrementPage = () => {
+        setUrlState({ page: (parseInt(state.page) || 0) + 1 })
+    }
+
+    return [state, setUrlState, incrementPage]
+}
