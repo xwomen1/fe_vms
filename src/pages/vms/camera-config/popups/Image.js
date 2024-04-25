71a5e181691b1024b1e8d18c2c15b6ab02a7c23c
minhhue <minhhue14072002@gmail.com>
2024-04-25 09:50:42 +0700

    camera config

--- src/pages/vms/camera-config/popups/Image.js
+++ src/pages/vms/camera-config/popups/Image.js
@@ -1,111 +1,44 @@
-import React, { useEffect, useState } from 'react'
-import { Autocomplete, TextField, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material'
-import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { FormControl, Grid, IconButton, InputLabel, MenuItem, Paper, Select, Switch } from '@mui/material'
+import Icon from 'src/@core/components/icon'
+import Link from 'next/link'
+import Tab from '@mui/material/Tab'
+import TabPanel from '@mui/lab/TabPanel'
+import TabContext from '@mui/lab/TabContext'
+import { styled } from '@mui/material/styles'
+import MuiTabList from '@mui/lab/TabList'
 import authConfig from 'src/configs/auth'
-import Swal from 'sweetalert2'
+import axios from 'axios'
+import TCP from './TCP-IP'
+import DDNs from './DDNS'
+import Port from './Port'
+import NTP from './NTP'
+
+import {
+  Autocomplete,
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Checkbox
+} from '@mui/material'
+import CustomTextField from 'src/@core/components/mui/text-field'
+
+
 
 const RolePopup = ({ open, onClose, onSelect, nvr }) => {
   const [selectedRole, setSelectedRole] = useState(null)
   const [groupName, setGroupName] = useState([])
   const [defaultGroup, setDefaultGroup] = useState(null)
   const [selectedGroupId, setSelectedGroupId] = useState(null) // Thêm trạng thái để lưu trữ id của nhóm được chọn
-  const [groupOptions, setGroupOptions] = useState([])
+  const [nvrs, setNvrs] = useState([])
   const [groupCode, setGroupCode] = useState([])
+  const [value, setValue] = useState('1')
 
-  const handleGroupChange = (event, newValue) => {
-    setDefaultGroup(newValue)
-    console.log(newValue.id)
-    if (newValue) {
-      setGroupName(newValue.name)
-      setGroupCode(newValue.code)
-      console.log(defaultGroup, 'nameee')
-    }
-  }
-
-  const createNewGroup = async () => {
-    try {
-      const token = localStorage.getItem(authConfig.storageTokenKeyName)
-
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
-
-      const response = await axios.post(
-        'https://dev-ivi.basesystem.one/smc/iam/api/v0/groups',
-        {
-          groupCode: groupCode,
-          groupName: groupName,
-          isPnLVGR: false
-        },
-        config
-      )
-
-      return response.data.data.groupId
-    } catch (error) {
-      throw error
-    }
-  }
-
-  const searchGroupId = async groupName => {
-    try {
-      const token = localStorage.getItem(authConfig.storageTokenKeyName)
-
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
-
-      const response = await axios.get(
-        `https://dev-ivi.basesystem.one/smc/iam/api/v0/groups/search?keyword=${groupName}`,
-        config
-      )
-      console.log(response.data.data[0].groupId)
-
-      return response.data.data[0].groupId
-    } catch (error) {
-      throw error
-    }
-  }
-
-  const handleRoleSelect = async () => {
-    try {
-      let newGroupId = await searchGroupId(defaultGroup.name)
-
-      if (!newGroupId) {
-        // Nếu nhóm chưa tồn tại, tạo mới
-        newGroupId = await createNewGroup()
-      }
-      const token = localStorage.getItem(authConfig.storageTokenKeyName)
-
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      }
-      await axios.post(
-        `https://dev-ivi.basesystem.one/smc/iam/api/v0/user-groups`,
-        {
-          groupId: newGroupId,
-          default: false,
-          leader: false,
-          userId: userId
-        },
-        config
-      )
-      Swal.fire('Thêm thành công', '', 'success')
-
-      onSelect(selectedRole)
-      onClose()
-    } catch (error) {
-      onClose()
-
-      Swal.fire('Đã xảy ra lỗi', error.response.data.message || 'Unknown error', 'error')
-
-      console.error('Error adding member to group:', error.message)
-    }
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
   }
 
   const handleCancel = () => {
@@ -124,11 +57,11 @@ const RolePopup = ({ open, onClose, onSelect, nvr }) => {
         }
 
         const response = await axios.get(
-          'https://sbs.basesystem.one/ivis/infrares/api/v0/regions?limit=25&page=1&parentID=f963e9d4-3d6b-45df-884d-15f93452f2a2',
+          `https://sbs.basesystem.one/ivis/vms/api/v0/nvrs/config/networkconfig/{idNVR}?idNVR=${nvr}`,
           config
         )
 
-        setGroupOptions(response.data.data)
+        setNvrs(response.data.data)
       } catch (error) {
         console.error('Error fetching data:', error)
       }
@@ -139,19 +72,134 @@ const RolePopup = ({ open, onClose, onSelect, nvr }) => {
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Add Role and {nvr}</DialogTitle>
+      <DialogTitle>Cấu hình hình ảnh
+</DialogTitle>
       <DialogContent>
-        <Autocomplete
-          value={defaultGroup}
-          onChange={(event, newValue) => handleGroupChange(event, newValue)}
-          options={groupOptions}
-          getOptionLabel={option => option.name}
-          renderInput={params => <TextField {...params} label='Nhóm' variant='outlined' />}
-        />
+      <div style={{ width: '100%' }}>
+      <Grid container spacing={2}>
+       <Grid item xs = {6}>
+       <img height='200' alt='error-illustration' src='/images/avatars/1.png' />
+       </Grid>
+       <Grid item xs= {6}>
+       <Grid container item component={Paper} style={{ backgroundColor: 'white', width: '100%', padding: '10px' }}>
+       <Grid>
+        Camera Name
+       </Grid>
+        <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel id='time-validity-label'>Camera Name</InputLabel>
+                <Select
+                  labelId='time-validity-label'
+                  id='time-validity-select'
+                 
+                >
+                  <MenuItem value='Custom'>Tuỳ chỉnh</MenuItem>
+                  <MenuItem value='Undefined'>Không xác định</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          
+        
+          
+    
+        </Grid>
+        <Grid container item component={Paper} style={{ backgroundColor: 'white', width: '100%', padding: '10px' }}>
+        <Grid item xs={12}>
+        Display Name
+            <Switch color='primary' />
+            
+          </Grid>
+          <Grid item xs={12}>
+        Display Date
+            <Switch color='primary' />
+            
+          </Grid>
+          <Grid item xs={12}>
+        Display Week
+            <Switch color='primary' />
+            
+          </Grid>
+             
+        
+          
+    
+        </Grid>
+        <Grid container item component={Paper} style={{ backgroundColor: 'white', width: '100%', padding: '10px' }}>
+       <Grid>
+        Date format
+       </Grid>
+        <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel id='time-validity-label'>Date format</InputLabel>
+                <Select
+                  labelId='time-validity-label'
+                  id='time-validity-select'
+                 
+                >
+                  <MenuItem value='Custom'>Tuỳ chỉnh</MenuItem>
+                  <MenuItem value='Undefined'>Không xác định</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          
+        
+          
+    
+        </Grid>
+        <Grid container item component={Paper} style={{ backgroundColor: 'white', width: '100%', padding: '10px' }}>
+       <Grid>
+        Time format
+       </Grid>
+        <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel id='time-validity-label'>Time format</InputLabel>
+                <Select
+                  labelId='time-validity-label'
+                  id='time-validity-select'
+                 
+                >
+                  <MenuItem value='Custom'>Tuỳ chỉnh</MenuItem>
+                  <MenuItem value='Undefined'>Không xác định</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          
+        
+          
+    
+        </Grid>
+        <Grid container item component={Paper} style={{ backgroundColor: 'white', width: '100%', padding: '10px' }}>
+       <Grid>
+        Display Mode
+       </Grid>
+        <Grid item xs={12}>
+              <FormControl fullWidth>
+                <InputLabel id='time-validity-label'>Display Mode</InputLabel>
+                <Select
+                  labelId='time-validity-label'
+                  id='time-validity-select'
+                 
+                >
+                  <MenuItem value='Custom'>Tuỳ chỉnh</MenuItem>
+                  <MenuItem value='Undefined'>Không xác định</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+          
+        
+          
+    
+        </Grid>
+       </Grid>
+       
+      </Grid>
+      <br />
+    </div>
+    
       </DialogContent>
       <DialogActions>
         <Button onClick={handleCancel}>Cancel</Button>
-        <Button onClick={handleRoleSelect}>OK</Button>
+        <Button>OK</Button>
       </DialogActions>
     </Dialog>
   )
