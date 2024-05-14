4a604389952a7d56c6cc8a7cfc51a707fd38ff90
Cường Nguyễn Nam <namcuongn1407@gmail.com>
2024-05-14 18:52:20 +0700

    update organizational-structure table and delete

--- src/pages/organizational-structure/index.js
+++ src/pages/organizational-structure/index.js
@@ -1,24 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import Card from '@mui/material/Card';
 import Grid from '@mui/material/Grid';
-import authConfig from 'src/configs/auth';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import TreeView from '@mui/lab/TreeView';
 import TreeItem from '@mui/lab/TreeItem';
 import axios from 'axios';
 import Icon from 'src/@core/components/icon';
-import { IconButton } from '@mui/material';
+import { Button, IconButton, Typography, Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import authConfig from 'src/configs/auth';
+import CustomTextField from 'src/@core/components/mui/text-field';
+import DeletePopup from './popup/delete';
 
 const OrganizationalStructure = () => {
   const [infra, setInfra] = useState([]);
-  const [page, setPage] = useState(1);
-  const [pageSize, setPageSize] = useState(10);
-  const [total, setTotal] = useState(0);
-  const [value, setValue] = useState('');
   const [selectedTab, setSelectedTab] = useState(0);
-  const [children, setChildren] = useState([]);
-  const [expandedNodes, setExpandedNodes] = useState({});
+  const [treeData, setTreeData] = useState({});
+  const [expandedNodes, setExpandedNodes] = useState([]);
+  const [childData, setChildData] = useState([]);
+  const [openPopup, setOpenPopup] = useState(false);
+  const [openPopupId, setOpenPopupId] = useState(null);
+
+  const handleOpenPopup = id => {
+    setOpenPopupId(id);
+    setOpenPopup(true);
+  };
+
+  const handleClose = () => {
+    setOpenPopup(false);
+  };
 
   const fetchFilter = async () => {
     try {
@@ -29,26 +38,57 @@ const OrganizationalStructure = () => {
           Authorization: `Bearer ${token}`
         },
         params: {
-          limit: pageSize,
-          page: page,
-          keyword: value
+          limit: 10,
+          page: 1,
+          keyword: ''
         }
       };
       const response = await axios.get('https://sbs.basesystem.one/ivis/infrares/api/v0/regions/adults', config);
       setInfra(response.data);
-      setTotal(response.data.total);
+
+      // Fetch child data for the first tab after infra data is loaded
+      if (response.data.length > 0) {
+        fetchChildData(response.data[0].id);
+        fetchTreeData(response.data[0].id);
+      }
     } catch (error) {
       console.error('Error fetching users:', error);
     }
   };
 
+  const handleSuccess = async () => {
+    await fetchChildData(infra[selectedTab]?.id);
+  };
+
+  const fetchChildData = async (parentId) => {
+    try {
+      const token = localStorage.getItem(authConfig.storageTokenKeyName);
+
+      const response = await axios.get(`https://sbs.basesystem.one/ivis/infrares/api/v0/regions/children-lv1/me/?parentId=${parentId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+
+      setChildData(response.data);
+      setTreeData((prevTreeData) => ({
+        ...prevTreeData,
+        [parentId]: response.data
+      }));
+    } catch (error) {
+      console.error('Error fetching children:', error);
+    }
+  };
+
   useEffect(() => {
     fetchFilter();
-  }, [page, pageSize, total, value]);
+  }, []);
 
-  const handleChangeTab = (event, newValue) => {
+  const handleChangeTab = async (event, newValue) => {
     setSelectedTab(newValue);
-    setChildren([]);
+    setTreeData({});
+    setExpandedNodes([]);
+    await fetchChildData(infra[newValue]?.id);
   };
 
   const fetchChildrenById = async (parentId) => {
@@ -60,92 +100,156 @@ const OrganizationalStructure = () => {
           Authorization: `Bearer ${token}`
         }
       });
-      
-return response.data;
+
+      return response.data;
     } catch (error) {
       console.error('Error fetching children:', error);
-      
-return [];
+
+      return [];
     }
   };
 
   const handleFetchChildren = async (nodeId) => {
-    const isExpanded = expandedNodes[nodeId];
+    const isExpanded = expandedNodes.includes(nodeId);
     if (isExpanded) {
-      const newExpandedNodes = { ...expandedNodes };
-      newExpandedNodes[nodeId] = false;
-      setExpandedNodes(newExpandedNodes);
-      setChildren(prevChildren => prevChildren.filter(child => child.parentId !== nodeId));
-      
-return;
+      setExpandedNodes(expandedNodes.filter(id => id !== nodeId));
+    } else {
+      const childrenData = await fetchChildrenById(nodeId);
+      setTreeData((prevTreeData) => ({
+        ...prevTreeData,
+        [nodeId]: childrenData
+      }));
+      setExpandedNodes([...expandedNodes, nodeId]);
     }
-    const childrenData = await fetchChildrenById(nodeId);
-    const newExpandedNodes = { ...expandedNodes };
-    newExpandedNodes[nodeId] = true;
-    setExpandedNodes(newExpandedNodes);
-    setChildren(prevChildren => [
-      ...prevChildren.filter(child => child.parentId !== nodeId),
-      { parentId: nodeId, children: childrenData }
-    ]);
   };
 
   const renderTreeItems = (nodes) => {
-    return nodes.map((parent) => (
-      <TreeItem
-        key={parent.parentId}
-        nodeId={parent.parentId}
-        label={infra.find(item => item.id === parent.parentId)?.name}
-        sx={{ marginLeft: '3%', marginTop: '2%' }}
-        icon={
-          <IconButton style={{ padding: '0px' }} onClick={() => handleFetchChildren(parent.parentId)}>
-            <Icon icon='tabler:chevron-right' />
-          </IconButton>
-        }
-      >
-        {parent.children.map((child) => (
-          <TreeItem
-            key={child.id}
-            nodeId={child.id}
-            label={child.name}
-            sx={{ marginLeft: '3%', marginTop: '2%' }}
-            icon={
-              child.isParent ? (
-                <IconButton style={{ padding: '0px' }} onClick={() => handleFetchChildren(child.id)}>
-                  <Icon icon='tabler:chevron-right' />
-                </IconButton>
-              ) : null
-            }
-          />
-        ))}
-      </TreeItem>
-    ));
+    return nodes.map((node) => {
+      const hasChildren = treeData[node.id] && treeData[node.id].length > 0;
+
+      return (
+        <TreeItem
+          key={node.id}
+          nodeId={node.id}
+          label={node.name}
+          sx={{ marginLeft: '3%', marginTop: '4%' }}
+          icon={
+            node.isParent ? (
+              <IconButton style={{ padding: '0px' }} onClick={() => handleFetchChildren(node.id)}>
+                <Icon icon={expandedNodes.includes(node.id) ? 'bi:chevron-down' : 'tabler:chevron-right'} />
+              </IconButton>
+            ) : null
+          }
+        >
+          {hasChildren && renderTreeItems(treeData[node.id])}
+        </TreeItem>
+      );
+    });
   };
+
+  const currentTabInfra = infra[selectedTab] || {};
+  const rootNodes = treeData[currentTabInfra.id] || [];
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Tabs value={selectedTab} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto">
-          {infra.map((infraItem, index) => (
-            <Tab key={index} label={infraItem.name} style={{ fontSize: '130%' }} />
-          ))}
-        </Tabs>
+        <Box display="flex" alignItems="center">
+          <Tabs value={selectedTab} onChange={handleChangeTab} variant="scrollable" scrollButtons="auto" style={{ marginRight: '30%' }}>
+            {infra.map((infraItem, index) => (
+              <Tab key={index} label={infraItem.name} style={{ fontSize: '120%' }} />
+            ))}
+          </Tabs>
+          <Box display="flex" alignItems="center">
+            <Typography variant="body2" style={{ marginRight: '16px' }}>
+              <Button variant='contained'>
+                Thêm mới
+              </Button>
+            </Typography>
+            <Typography variant="body2">
+              <Button variant='contained'>
+                Xóa
+              </Button>
+            </Typography>
+          </Box>
+        </Box>
       </Grid>
       {infra.length > 0 && (
-        <Grid item xs={12}>
-          <Grid item xs={3}>
-            <IconButton style={{ padding: '0px' }} onClick={() => handleFetchChildren(infra[selectedTab].id)}>
-              <Icon icon='tabler:chevron-right' />
-            </IconButton>
-            {infra[selectedTab].name}
-            <TreeView
-              defaultCollapseIcon={<Icon icon="bi:chevron-down" />}
-              defaultExpandIcon={<Icon icon="bi:chevron-right" />}
-              expanded={Object.keys(expandedNodes)}
-            >
-              {renderTreeItems(children)}
-            </TreeView>
+        <Grid container spacing={2} style={{ height: '100%' }}>
+          <Grid item xs={2.5} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Paper elevation={3} style={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <TreeView
+                defaultCollapseIcon={<Icon icon="bi:chevron-down" />}
+                defaultExpandIcon={<Icon icon="bi:chevron-right" />}
+                expanded={expandedNodes}
+              >
+                {renderTreeItems([{ id: currentTabInfra.id, name: currentTabInfra.name, isParent: true }])}
+              </TreeView>
+            </Paper>
+          </Grid>
+          <Grid item xs={9.5} style={{ display: 'flex', flexDirection: 'column' }}>
+            <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+              <CustomTextField
+                label='Tên'
+                type='text'
+                value={currentTabInfra.name}
+                fullWidth
+                style={{ marginBottom: '16px' }}
+              />
+              <CustomTextField
+                label='Mã'
+                type='text'
+                value={currentTabInfra.name}
+                fullWidth
+                style={{ marginBottom: '16px' }}
+              />
+              <CustomTextField
+                label='Ghi chú'
+                type='text'
+                value={currentTabInfra.detail}
+                fullWidth
+              />
+            </Paper>
+            <Paper elevation={3} style={{ padding: '16px', flexGrow: 1 }}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>STT</TableCell>
+                      <TableCell>Tên địa phương</TableCell>
+                      <TableCell>Mã địa phương</TableCell>
+                      <TableCell>Thao tác</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {childData.length > 0 && childData.map((child, index) => (
+                      <TableRow key={child.id}>
+                        <TableCell>{index + 1}</TableCell>
+                        <TableCell>{child.name}</TableCell>
+                        <TableCell>{child.name}</TableCell>
+                        <TableCell sx={{ padding: '16px' }}>
+                          <IconButton size='small'>
+                            <Icon icon='tabler:edit' />
+                          </IconButton>
+                          <IconButton onClick={() => handleOpenPopup(child.id)}>
+                            <Icon icon='tabler:trash' />
+                          </IconButton>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Paper>
           </Grid>
         </Grid>
+      )}
+      {openPopup && (
+        <DeletePopup
+          open={openPopup}
+          onClose={handleClose}
+          id={openPopupId}
+          onSuccess={handleSuccess}
+        />
       )}
     </Grid>
   );
