361bfe996fc0c0f6d5c75b1d2ca371391c8925ac
dat-th23 <dat.th1230@gmail.com>
2024-05-15 15:43:03 +0700

    ai config + event ai 2

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -105,6 +105,8 @@ const AuthProvider = ({ children }) => {
     try {
       const token = localStorage.getItem(authConfig.storageTokenKeyName)
 
+      console.log('token', token)
+
       // ** Đặt header Authorization bằng token
 
       const params = {
@@ -128,11 +130,13 @@ const AuthProvider = ({ children }) => {
       .then(async response => {
         const tokenReturn = response.data.access_token
         const refreshToken = response.data.refresh_token
+        console.log('tokenreturn', tokenReturn)
         localStorage.setItem(authConfig.storageTokenKeyName, tokenReturn)
         localStorage.setItem(authConfig.onTokenExpiration, refreshToken)
 
         const returnUrl = router.query.returnUrl
         setExpire(response.data.expires_in)
+        console.log(response.data.expires_in)
 
         const apiUser = {
           username: params.username,
@@ -143,6 +147,7 @@ const AuthProvider = ({ children }) => {
         setLoading(false)
 
         const redirectURL = returnUrl && returnUrl !== '/' ? returnUrl : '/'
+        console.log('URL', redirectURL)
         router.replace(redirectURL)
       })
       .catch(err => {
