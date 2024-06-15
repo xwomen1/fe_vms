57fc8436c7125c33f84a2bbf287e269c39ccd42d
MinhHue <minhhue14072002@gmail.com>
2024-06-15 18:09:31 +0700

    update group user

--- src/pages/apps/user/list/index.js
+++ src/pages/apps/user/list/index.js
@@ -1,389 +1,64 @@
-import { useState, useEffect, useCallback } from 'react'
-import Card from '@mui/material/Card'
-import Menu from '@mui/material/Menu'
-import Grid from '@mui/material/Grid'
-import MenuItem from '@mui/material/MenuItem'
-import TreeView from '@mui/lab/TreeView'
-import TreeItem from '@mui/lab/TreeItem'
-import TableBody from '@mui/material/TableBody'
-import TableCell from '@mui/material/TableCell'
-import TableHead from '@mui/material/TableHead'
-import TableRow from '@mui/material/TableRow'
-import authConfig from 'src/configs/auth'
-import Table from '@mui/material/Table'
-import Paper from '@mui/material/Paper'
-import Pagination from '@mui/material/Pagination'
-import Icon from 'src/@core/components/icon'
-import { IconButton } from '@mui/material'
-import Swal from 'sweetalert2'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchData } from 'src/store/apps/user'
-import { useRouter } from 'next/router'
-import axios from 'axios'
-import TableHeader from 'src/views/apps/user/list/TableHeader'
-import CustomTextField from 'src/@core/components/mui/text-field'
-import UserDetails from '../detail/index'
-import Link from 'next/link'
-import { fetchChatsContacts } from 'src/store/apps/chat'
+import { Grid } from '@mui/material'
 
-const UserList = ({ apiData }) => {
-  const [value, setValue] = useState('')
-  const [valueGroup, setValueGroup] = useState('')
+import ListUser from './ListUser'
+import Tab from '@mui/material/Tab'
+import TabPanel from '@mui/lab/TabPanel'
+import { styled } from '@mui/material/styles'
+import MuiTabList from '@mui/lab/TabList'
+import TabContext from '@mui/lab/TabContext'
+import { useState } from 'react'
+import TreeViewGroup from './treeview'
 
-  const [addUserOpen, setAddUserOpen] = useState(false)
-  const [userData, setUserData] = useState([])
-  const [total, setTotal] = useState([1])
-  const [selectedGroups, setSelectedGroups] = useState([])
-  const [page, setPage] = useState(1)
-  const [pageSize, setPageSize] = useState(25)
-  const pageSizeOptions = [25, 50, 100]
-  const [groups, setGroups] = useState([])
-  const [anchorEl, setAnchorEl] = useState(null)
-  const router = useRouter()
-  const [contractName, setContractName] = useState('')
-  const [contractTypes, setContractTypes] = useState({})
-
-  const handlePageChange = newPage => {
-    setPage(newPage)
-  }
-
-  const handleViewDetails = userId => {
-    router.push(`/${userId}`)
-  }
-
-  function showAlertConfirm(options, intl) {
-    const defaultProps = {
-      title: intl ? intl.formatMessage({ id: 'app.title.confirm' }) : 'Xác nhận',
-      imageWidth: 213,
-      showCancelButton: true,
-      showCloseButton: true,
-      showConfirmButton: true,
-      focusCancel: true,
-      reverseButtons: true,
-      confirmButtonText: intl ? intl.formatMessage({ id: 'app.button.OK' }) : 'Đồng ý',
-      cancelButtonText: intl ? intl.formatMessage({ id: 'app.button.cancel' }) : 'Hủy',
-      customClass: {
-        content: 'content-class',
-        confirmButton: 'swal-btn-confirm'
-      }
-    }
-
-    return Swal.fire({ ...defaultProps, ...options })
-  }
-
-  const handleOpenMenu = event => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleCloseMenu = () => {
-    setAnchorEl(null)
-  }
-
-  const handleSelectPageSize = size => {
-    setPageSize(size)
-    setPage(1)
-    handleCloseMenu()
-  }
-
-  const fetchRegionName = async regionId => {
-    try {
-      const response = await axios.get(`https://sbs.basesystem.one/ivis/infrares/api/v0/regions/${regionId}`)
-
-      return response.data.name
-    } catch (error) {
-      console.error('Error fetching region name:', error)
-
-      return ''
+const TabList = styled(MuiTabList)(({ theme }) => ({
+  borderBottom: '0 !important',
+  '&, & .MuiTabs-scroller': {
+    boxSizing: 'content-box',
+    padding: theme.spacing(1.25, 1.25, 2),
+    margin: `${theme.spacing(-1.25, -1.25, -2)} !important`
+  },
+  '& .MuiTabs-indicator': {
+    display: 'none'
+  },
+  '& .Mui-selected': {
+    boxShadow: theme.shadows[2],
+    backgroundColor: theme.palette.primary.main,
+    color: `${theme.palette.common.white} !important`
+  },
+  '& .MuiTab-root': {
+    lineHeight: 1,
+    borderRadius: theme.shape.borderRadius,
+    '&:hover': {
+      color: theme.palette.primary.main
     }
   }
-  useEffect(() => {
-    const fetchAllRegionNames = async () => {
-      const newContractTypes = {}
-      for (const user of userData) {
-        if (user.contractType) {
-          const regionName = await fetchRegionName(user.contractType)
-          newContractTypes[user.contractType] = regionName
-        }
-      }
-      setContractTypes(newContractTypes)
-    }
+}))
 
-    fetchAllRegionNames()
-  }, [userData])
+const Caller = () => {
+  const [value, setValue] = useState('1')
 
-  const GroupCheckbox = ({ group, checked, onChange }) => {
-    return (
-      <div>
-        <input
-          type='checkbox'
-          id={`group-${group.groupId}`}
-          checked={checked}
-          onChange={e => onChange(group.groupId, e.target.checked)}
-        />
-        <label htmlFor={`group-${group.groupId}`}>{group.groupName}</label>
-      </div>
-    )
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
   }
-
-  const handleGroupCheckboxChange = (groupId, checked) => {
-    if (checked) {
-      setSelectedGroups(prevGroups => [...prevGroups, { groupId }])
-    } else {
-      setSelectedGroups(prevGroups => prevGroups.filter(g => g.groupId !== groupId))
-    }
-  }
-  useEffect(() => {
-    const fetchGroupData = async () => {
-      try {
-        const token = localStorage.getItem(authConfig.storageTokenKeyName)
-        console.log('token', token)
-
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-          params: {
-            keyword: valueGroup
-          }
-        }
-        const response = await axios.get('https://dev-ivi.basesystem.one/smc/iam/api/v0/groups/search', config)
-        const dataWithChildren = addChildrenField(response.data)
-        const rootGroups = findRootGroups(dataWithChildren)
-        setGroups(rootGroups)
-      } catch (error) {
-        console.error('Error fetching data:', error)
-      }
-    }
-
-    fetchGroupData()
-  }, [pageSize, valueGroup])
-
-  const addChildrenField = (data, parentId = null) => {
-    return data.map(group => {
-      const children = data.filter(child => child.parentId === group.groupId)
-      if (children.length > 0) {
-        group.children = children
-      }
-
-      return group
-    })
-  }
-
-  const renderGroup = group => (
-    <TreeItem
-      key={group.groupId}
-      nodeId={group.groupId}
-      label={
-        <GroupCheckbox
-          group={group}
-          checked={selectedGroups.some(g => g.groupId === group.groupId)}
-          onChange={handleGroupCheckboxChange}
-        />
-      }
-      style={{ marginTop: '5%' }}
-    >
-      {group.children && group.children.map(childGroup => renderGroup(childGroup))}
-    </TreeItem>
-  )
-
-  const findRootGroups = data => {
-    const rootGroups = []
-    data.forEach(group => {
-      if (!data.some(item => item.groupId === group.parentId)) {
-        rootGroups.push(group)
-      }
-    })
-
-    return rootGroups
-  }
-
-  const handleFilter = useCallback(val => {
-    setValue(val)
-  }, [])
-
-  const handleFilterGroup = event => {
-    setValueGroup(event)
-  }
-
-  console.log(total, 'totalpage')
-
-  const handleDelete = idDelete => {
-    showAlertConfirm({
-      text: 'Bạn có chắc chắn muốn xóa?'
-    }).then(({ value }) => {
-      if (value) {
-        const token = localStorage.getItem(authConfig.storageTokenKeyName)
-        if (!token) {
-          return
-        }
-
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        }
-        let urlDelete = `https://dev-ivi.basesystem.one/smc/iam/api/v0/users/${idDelete}`
-        axios
-          .delete(urlDelete, config)
-          .then(() => {
-            Swal.fire('Xóa thành công', '', 'success')
-            const updatedData = userData.filter(user => user.userId !== idDelete)
-            setUserData(updatedData)
-            fetchData()
-          })
-          .catch(err => {
-            Swal.fire('Đã xảy ra lỗi', err.message, 'error')
-          })
-      }
-    })
-  }
-
-  const toggleAddUserDrawer = () => setAddUserOpen(!addUserOpen)
-  useEffect(() => {
-    const fetchFilteredOrAllUsers = async () => {
-      try {
-        const token = localStorage.getItem(authConfig.storageTokenKeyName)
-
-        const config = {
-          headers: {
-            Authorization: `Bearer ${token}`
-          },
-          params: {
-            limit: pageSize,
-            page: page,
-            keyword: value
-          }
-        }
-        let url
-        if (selectedGroups.length > 0) {
-          url = `https://dev-ivi.basesystem.one/smc/iam/api/v0/users/search?groupIds=${selectedGroups
-            .map(g => g.groupId)
-            .join(',')}`
-        } else {
-          url = 'https://dev-ivi.basesystem.one/smc/iam/api/v0/users/search'
-        }
-        const response = await axios.get(url, config)
-        setUserData(response.data.rows)
-        setContractName(response.data.rows.contractType)
-        setTotal(response.data.totalPage)
-      } catch (error) {
-        console.error('Error fetching users:', error)
-      }
-    }
-    fetchFilteredOrAllUsers()
-  }, [selectedGroups, page, pageSize, total, value])
 
   return (
     <Grid style={{ minWidth: '1000px' }}>
-      <Card>
-        <TableHeader value={value} handleFilter={handleFilter} toggle={toggleAddUserDrawer} />
-        <Grid container spacing={2}>
-          {/* <Grid item xs={0.1}></Grid> */}
-          <Grid item xs={0.2}></Grid>
-
-          <Grid item xs={2} component={Paper}>
-            <div>
-              <CustomTextField
-                value={valueGroup}
-                sx={{ mr: 4 }}
-                placeholder='Search Group'
-                onChange={e => handleFilterGroup(e.target.value)}
-              />
-              <TreeView
-                sx={{ minHeight: 240 }}
-                defaultExpandIcon={<Icon icon='tabler:chevron-right' />}
-                defaultCollapseIcon={<Icon icon='tabler:chevron-down' />}
-              >
-                {groups.map(rootGroup => renderGroup(rootGroup))}
-              </TreeView>
-            </div>
-          </Grid>
-          <Grid item xs={9.8}>
-            <Paper elevation={3}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell sx={{ padding: '16px' }}>STT</TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Mã định danh</TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Full Name</TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Email</TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Số điện thoại </TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Đơn vị</TableCell>
-                    <TableCell sx={{ padding: '16px' }}>Loại hợp đồng</TableCell>
-
-                    <TableCell sx={{ padding: '16px' }}>Hành động</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {userData.map((user, index) => (
-                    <TableRow key={user.userId}>
-                      <TableCell sx={{ padding: '16px' }}>{(page - 1) * pageSize + index + 1} </TableCell>
-                      <TableCell sx={{ padding: '16px' }}>{user.accessCode}</TableCell>
-                      <TableCell sx={{ padding: '16px' }}>{user.fullName}</TableCell>
-                      <TableCell sx={{ padding: '16px' }}>{user.email}</TableCell>
-                      <TableCell sx={{ padding: '16px' }}>{user.phoneNumber}</TableCell>
-                      <TableCell sx={{ padding: '16px' }}>{user.userGroup[0]?.groupName}</TableCell>
-                      <TableCell sx={{ padding: '16px' }}>
-                        {contractTypes[user.contractType] || 'Không thời hạn'}
-                      </TableCell>
-
-                      <TableCell sx={{ padding: '16px' }}>
-                        <Grid container spacing={2}>
-                          <IconButton
-                            size='small'
-                            component={Link}
-                            href={`/apps/user/detail/${user.userId}`}
-                            sx={{ color: 'text.secondary' }}
-                          >
-                            <Icon icon='tabler:info-circle' />
-                          </IconButton>
-                          <IconButton onClick={() => handleDelete(user.userId)}>
-                            <Icon icon='tabler:trash' />
-                          </IconButton>
-                        </Grid>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Paper>
-
-            <br></br>
-            <Grid container spacing={2} style={{ padding: 10 }}>
-              <Grid item xs={3}></Grid>
-              <Grid item xs={1.5} style={{ padding: 0 }}>
-                <IconButton onClick={handleOpenMenu}>
-                  <Icon icon='tabler:selector' />
-                  <p style={{ fontSize: 15 }}>{pageSize} dòng/trang</p>
-                </IconButton>
-                <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleCloseMenu}>
-                  {pageSizeOptions.map(size => (
-                    <MenuItem key={size} onClick={() => handleSelectPageSize(size)}>
-                      {size}
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Grid>
-              <Grid item xs={6}>
-                <Pagination count={total} color='primary' onChange={(event, page) => handlePageChange(page)} />
-              </Grid>
-            </Grid>
-          </Grid>
+      <TabContext value={value}>
+        <Grid>
+          {' '}
+          <TabList onChange={handleChange} aria-label='customized tabs example'>
+            <Tab value='1' label='Danh sách người dùng' />
+            <Tab value='2' label='Đồng bộ nhóm người dùng' />
+          </TabList>
         </Grid>
-      </Card>
+        <TabPanel value='1'>
+          <ListUser />
+        </TabPanel>
+        <TabPanel value='2'>
+          <TreeViewGroup />
+        </TabPanel>
+      </TabContext>
     </Grid>
   )
 }
 
-export const getStaticProps = async () => {
-  const res = await axios.get('/cards/statistics')
-  const apiData = res.data
-
-  return {
-    props: {
-      apiData
-    }
-  }
-}
-
-export default UserList
+export default Caller
---
+++ src/pages/apps/user/list/test.js
@@ -0,0 +1,314 @@
+import { useState, useEffect, useCallback } from 'react'
+import Card from '@mui/material/Card'
+import Menu from '@mui/material/Menu'
+import Grid from '@mui/material/Grid'
+import MenuItem from '@mui/material/MenuItem'
+import TreeView from '@mui/lab/TreeView'
+import TreeItem from '@mui/lab/TreeItem'
+import TableBody from '@mui/material/TableBody'
+import TableCell from '@mui/material/TableCell'
+import TableHead from '@mui/material/TableHead'
+import TableRow from '@mui/material/TableRow'
+import authConfig from 'src/configs/auth'
+import Table from '@mui/material/Table'
+import Paper from '@mui/material/Paper'
+import Pagination from '@mui/material/Pagination'
+import Icon from 'src/@core/components/icon'
+import { IconButton, Typography, Box } from '@mui/material'
+import Swal from 'sweetalert2'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchData } from 'src/store/apps/user'
+import { useRouter } from 'next/router'
+import axios from 'axios'
+import TableHeader from 'src/views/apps/user/list/TableHeader'
+import CustomTextField from 'src/@core/components/mui/text-field'
+import UserDetails from '../detail/index'
+import Link from 'next/link'
+import { fetchChatsContacts } from 'src/store/apps/chat'
+
+const UserList = ({ apiData }) => {
+  const [value, setValue] = useState('')
+  const [valueGroup, setValueGroup] = useState('')
+
+  const [addUserOpen, setAddUserOpen] = useState(false)
+  const [userData, setUserData] = useState([])
+  const [total, setTotal] = useState([1])
+  const [selectedGroups, setSelectedGroups] = useState([])
+  const [page, setPage] = useState(1)
+  const [pageSize, setPageSize] = useState(25)
+  const pageSizeOptions = [25, 50, 100]
+  const [groups, setGroups] = useState([])
+  const [anchorEl, setAnchorEl] = useState(null)
+  const router = useRouter()
+  const [contractName, setContractName] = useState('')
+  const [contractTypes, setContractTypes] = useState({})
+
+  const handlePageChange = newPage => {
+    setPage(newPage)
+  }
+
+  const handleViewDetails = userId => {
+    router.push(`/${userId}`)
+  }
+
+  function showAlertConfirm(options, intl) {
+    const defaultProps = {
+      title: intl ? intl.formatMessage({ id: 'app.title.confirm' }) : 'Xác nhận',
+      imageWidth: 213,
+      showCancelButton: true,
+      showCloseButton: true,
+      showConfirmButton: true,
+      focusCancel: true,
+      reverseButtons: true,
+      confirmButtonText: intl ? intl.formatMessage({ id: 'app.button.OK' }) : 'Đồng ý',
+      cancelButtonText: intl ? intl.formatMessage({ id: 'app.button.cancel' }) : 'Hủy',
+      customClass: {
+        content: 'content-class',
+        confirmButton: 'swal-btn-confirm'
+      }
+    }
+
+    return Swal.fire({ ...defaultProps, ...options })
+  }
+
+  const handleOpenMenu = event => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null)
+  }
+
+  const handleSelectPageSize = size => {
+    setPageSize(size)
+    setPage(1)
+    handleCloseMenu()
+  }
+
+  const fetchRegionName = async regionId => {
+    try {
+      const response = await axios.get(`https://sbs.basesystem.one/ivis/infrares/api/v0/regions/${regionId}`)
+
+      return response.data.name
+    } catch (error) {
+      console.error('Error fetching region name:', error)
+
+      return ''
+    }
+  }
+  useEffect(() => {
+    const fetchAllRegionNames = async () => {
+      const newContractTypes = {}
+      for (const user of userData) {
+        if (user.contractType) {
+          const regionName = await fetchRegionName(user.contractType)
+          newContractTypes[user.contractType] = regionName
+        }
+      }
+      setContractTypes(newContractTypes)
+    }
+
+    fetchAllRegionNames()
+  }, [userData])
+
+  const GroupCheckbox = ({ group, checked, onChange }) => {
+    return (
+      <Box display='flex' alignItems='center' style={{ marginLeft: '5%' }}>
+        <Typography>
+          {' '}
+          <label htmlFor={`group-${group.id}`}>{group.name}</label>
+        </Typography>{' '}
+        <IconButton style={{ marginLeft: 'auto' }} size='small' onClick={() => handleDeleteInf(group.id)}>
+          <Icon icon='tabler:trash' />
+        </IconButton>
+      </Box>
+    )
+  }
+
+  const handleGroupCheckboxChange = (id, checked) => {
+    if (checked) {
+      setSelectedGroups(prevGroups => [...prevGroups, { id }])
+    } else {
+      setSelectedGroups(prevGroups => prevGroups.filter(g => g.id !== id))
+    }
+  }
+
+  const fetchGroupData = async () => {
+    try {
+      const token = localStorage.getItem(authConfig.storageTokenKeyName)
+      console.log('token', token)
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        },
+        params: {
+          keyword: valueGroup
+        }
+      }
+
+      const response = await axios.get(
+        'https://sbs.basesystem.one/ivis/infrares/api/v0/regions/parentsID?parentID=f963e9d4-3d6b-45df-884d-15f93452f2a2',
+        config
+      )
+      const dataWithChildren = addChildrenField(response.data)
+      const rootGroups = findRootGroups(dataWithChildren)
+      setGroups(rootGroups)
+    } catch (error) {
+      console.error('Error fetching data:', error)
+    }
+  }
+
+  useEffect(() => {
+    const fetchGroupData = async () => {
+      try {
+        const token = localStorage.getItem(authConfig.storageTokenKeyName)
+        console.log('token', token)
+
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`
+          },
+          params: {
+            keyword: valueGroup
+          }
+        }
+
+        const response = await axios.get(
+          'https://sbs.basesystem.one/ivis/infrares/api/v0/regions/parentsID?parentID=f963e9d4-3d6b-45df-884d-15f93452f2a2',
+          config
+        )
+        const dataWithChildren = addChildrenField(response.data)
+        const rootGroups = findRootGroups(dataWithChildren)
+        setGroups(rootGroups)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchGroupData()
+  }, [pageSize, valueGroup])
+
+  const addChildrenField = (data, parentID = null) => {
+    return data.map(group => {
+      const children = data.filter(child => child.parentID === group.id)
+      if (children.length > 0) {
+        group.children = children
+      }
+
+      return group
+    })
+  }
+
+  const handleDeleteInf = id => {
+    showAlertConfirm({
+      text: 'Bạn có chắc chắn muốn xóa?'
+    }).then(({ value }) => {
+      if (value) {
+        const token = localStorage.getItem(authConfig.storageTokenKeyName)
+        if (!token) {
+          return
+        }
+
+        const config = {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+        let urlDelete = `https://sbs.basesystem.one/ivis/infrares/api/v0/regions/${id}`
+        axios
+          .delete(urlDelete, config)
+          .then(() => {
+            Swal.fire('Xóa thành công', '', 'success')
+            fetchGroupData()
+          })
+          .catch(err => {
+            console.log(err)
+            Swal.fire('Đã xảy ra lỗi', err?.response?.statusText, 'error')
+          })
+      }
+    })
+  }
+
+  const renderGroup = group => (
+    <TreeItem
+      key={group.id}
+      nodeId={group.id}
+      label={
+        <GroupCheckbox
+          group={group}
+          checked={selectedGroups.some(g => g.id === group.id)}
+          onChange={handleGroupCheckboxChange}
+        />
+      }
+      style={{ marginTop: '5%' }}
+    >
+      {group.children && group.children.map(childGroup => renderGroup(childGroup))}
+    </TreeItem>
+  )
+
+  const findRootGroups = data => {
+    const rootGroups = []
+    data.forEach(group => {
+      if (!data.some(item => item.id === group.parentID)) {
+        rootGroups.push(group)
+      }
+    })
+
+    return rootGroups
+  }
+
+  const handleFilter = useCallback(val => {
+    setValue(val)
+  }, [])
+
+  const handleFilterGroup = event => {
+    setValueGroup(event)
+  }
+
+  console.log(total, 'totalpage')
+
+  const toggleAddUserDrawer = () => setAddUserOpen(!addUserOpen)
+
+  return (
+    <Grid style={{ minWidth: '1000px' }}>
+      <Card>
+        <Grid container spacing={2}>
+          {/* <Grid item xs={0.1}></Grid> */}
+          <Grid item xs={0.2}></Grid>
+
+          <Grid item xs={2} component={Paper}>
+            <div>
+              <CustomTextField
+                value={valueGroup}
+                sx={{ mr: 4 }}
+                placeholder='Search Group'
+                onChange={e => handleFilterGroup(e.target.value)}
+              />
+              <TreeView
+                sx={{ minHeight: 240 }}
+                defaultExpandIcon={<Icon icon='tabler:chevron-right' />}
+                defaultCollapseIcon={<Icon icon='tabler:chevron-down' />}
+              >
+                {groups.map(rootGroup => renderGroup(rootGroup))}
+              </TreeView>
+            </div>
+          </Grid>
+        </Grid>
+      </Card>
+    </Grid>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await axios.get('/cards/statistics')
+  const apiData = res.data
+
+  return {
+    props: {
+      apiData
+    }
+  }
+}
+
+export default UserList
