3bbc95008f5e64c1c1d1a09fb9527f8b27132991
MinhHue <minhhue14072002@gmail.com>
2024-06-27 14:32:46 +0700

    conflict code

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -12,134 +12,9 @@ const navigation = () => {
       icon: 'tabler:calendar-event'
     },
     {
-      path: '/',
-      title: 'Quản lý hiệu suất',
-      icon: 'tabler:lock-access',
-      children: [
-        {
-          title: 'Thiết lập bộ KPI',
-          path: '/kpi',
-          icon: 'tabler:calendar-event'
-        },
-        {
-          title: 'Thiết bị',
-          path: '/device-management',
-          icon: 'tabler:accessible'
-        },
-        {
-          path: '/pages/access-rights/list',
-          title: 'Quyền truy cập',
-          icon: 'tabler:calendar-month'
-        },
-
-        {
-          title: 'Chấm công',
-          path: '/access-control/time-keeping',
-          icon: 'tabler:lock-access'
-        },
-        {
-          path: '/pages/report-month/list',
-          title: 'Báo cáo tháng',
-          icon: 'tabler:id'
-        }
-
-        // {
-        //   path: '/pages/access-rights/list',
-        //   title: 'Quyền truy cập',
-        //   icon: 'tabler:shield'
-        // },
-        // {
-        //   path: '/pages/access-rights/list',
-        //   title: 'Thống kê',
-        //   icon: 'tabler:statistics/time?tab=%2Fstatistics%2Ftime'
-        // },
-        // {
-        //   path: '/pages/access-rights/list',
-        //   title: 'Hẹn lịch',
-        //   icon: 'tabler:guest-registrations'
-        // }
-      ]
-    },
-    {
-      path: '/asset',
-      title: 'Hệ thống VMS',
-      icon: 'tabler:camera',
-      children: [
-        {
-          title: 'Xem trực tiếp',
-          path: '/vms/live-view',
-          icon: 'tabler:video'
-        },
-
-        // {
-        //   title: 'Xem trực tiếp 2',
-        //   path: '/vms/live-view-p2p-private'
-        // },
-        {
-          title: 'Xem lại',
-          path: '/vms/playback-camera',
-          icon: 'tabler:video-minus'
-        },
-        {
-          title: 'Trích xuất clip',
-          path: '/vms/clip-extraction',
-          icon: 'tabler:file-export'
-        },
-        {
-          title: 'Nhóm camera',
-          path: '/vms/camera-group',
-          icon: 'tabler:schema'
-        },
-        {
-          title: 'Sự kiện AI',
-          path: '/vms/events',
-          icon: 'tabler:timeline-event-exclamation'
-        },
-        {
-          title: 'Cấu hình AI',
-          path: '/vms/ai-config',
-          icon: 'tabler:settings'
-        },
-        {
-          title: 'Sự cố hệ thống',
-          path: '/vms/error-system',
-          icon: 'tabler:id-badge-off'
-        },
-        {
-          title: 'Danh sách camera',
-          path: '/vms/camera-config',
-          icon: 'tabler:device-cctv'
-        },
-        {
-          title: 'Danh sách nvr',
-          path: '/vms/nvr-config',
-          icon: 'tabler:server-2'
-        },
-        {
-          title: 'Smart NVR',
-          path: '/vms/smart-nvr',
-          icon: 'tabler:accessible'
-        },
-        {
-          path: '/pages/face_management/list',
-          title: 'Quản lý khuôn mặt',
-          icon: 'tabler:face-id'
-        },
-        {
-          path: '/pages/car_management/list',
-          title: 'Quản lý Biển số',
-          icon: 'tabler:id-badge-2'
-        }
-
-        // {
-        //   title: 'Thư viện media',
-        //   path: '/vms/library'
-        // },
-        // {
-        //   title: 'Bản đồ số',
-        //   path: '/vms/map'
-        // }
-      ]
+      title: 'Thiết bị',
+      path: '/device-management',
+      icon: 'tabler:accessible'
     },
     {
       path: '/pages/access-rights/list',
@@ -289,66 +164,27 @@ const navigation = () => {
     {
       title: 'Người dùng',
       icon: 'tabler:user',
-      children: [
-        {
-          title: 'Người dùng',
-          icon: 'tabler:user',
-          path: '/apps/user/list'
-        },
-
-        {
-          title: 'Hồ sơ ứng tuyển',
-          icon: 'tabler:building',
-          path: '/cv'
-        },
-        {
-          title: 'Cơ cấu tổ chức',
-          icon: 'tabler:building',
-          path: '/organizational-structure'
-        },
-        {
-          title: 'Vai Trò',
-          icon: 'tabler:users',
-          path: '/apps/roles'
-        },
-        {
-          title: 'Bảng lương',
-          icon: 'tabler:coin-pound',
-          path: '/salary'
-        },
-        {
-          title: 'Quy định về lương',
-          icon: 'tabler:moneybag',
-          path: '/salaryRule'
-        }
-      ]
+      path: '/apps/user/list'
     },
     {
-      title: 'Cài đặt nâng cao',
-      path: '/alert-management',
-      icon: 'tabler:settings-automation',
-      children: [
-        {
-          title: 'Quản lý nhóm quyền truy cập',
-          path: '/mappingACToInf',
-          icon: 'tabler:devices'
-        },
-        {
-          title: 'Cảnh báo hệ thống',
-          path: '/alert-management',
-          icon: 'tabler:zoom-exclamation'
-        },
-        {
-          title: 'OTA',
-          path: '/ota',
-          icon: 'tabler:download'
-        },
-        {
-          title: 'Logs',
-          path: '/logs',
-          icon: 'tabler:article'
-        }
-      ]
+      title: 'Cơ cấu tổ chức',
+      icon: 'tabler:building',
+      path: '/organizational-structure'
+    },
+    {
+      title: 'Vai Trò',
+      icon: 'tabler:users',
+      path: '/apps/roles'
+    },
+    {
+      title: 'Bảng lương',
+      icon: 'tabler:coin-pound',
+      path: '/salary'
+    },
+    {
+      title: 'Quy định về lương',
+      icon: 'tabler:moneybag',
+      path: '/salaryRule'
     }
 
     // {
