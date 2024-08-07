46c62268be727bf5e6f49b69b536bc101b87a327
nguyenngoctu9801 <nguyenngoctu9801@gmail.com>
2024-08-07 12:08:43 +0700

    Update Color

--- src/pages/vms/camera-config/add.js
+++ src/pages/vms/camera-config/add.js
@@ -17,7 +17,7 @@ import axios from 'axios'
 import CustomTextField from 'src/@core/components/mui/text-field'
 import { Radio, RadioGroup, FormControlLabel } from '@mui/material'
 import toast from 'react-hot-toast'
-
+import CustomChip from 'src/@core/components/mui/chip'
 import Checkbox from '@mui/material/Checkbox'
 import PopupScanHik from './popups/AddHik'
 import PopupScanDungIP from './popups/AddDungIP'
@@ -614,6 +614,53 @@ const Add = ({ apiData }) => {
     }
   }, [assettypeStatus])
 
+  const fetchDataReload = async id => {
+    try {
+      const token = localStorage.getItem(authConfig.storageTokenKeyName)
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }
+
+      const response = await axios.get(
+        `https://sbs.basesystem.one/ivis/vms/api/v0/device/camera/synchronize?camera_id=${id}`,
+        config
+      )
+      Swal.fire({
+        title: 'Reaload hành công!',
+        text: response?.message,
+        icon: 'success',
+        willOpen: () => {
+          const confirmButton = Swal.getConfirmButton()
+          if (confirmButton) {
+            confirmButton.style.backgroundColor = '#FF9F43'
+            confirmButton.style.color = 'white'
+          }
+        }
+      })
+    } catch (error) {
+      console.error('Error fetching users:', error)
+      Swal.fire({
+        title: 'Error!',
+        text: error.response?.data?.message,
+        icon: 'error',
+        willOpen: () => {
+          const confirmButton = Swal.getConfirmButton()
+          if (confirmButton) {
+            confirmButton.style.backgroundColor = '#FF9F43'
+            confirmButton.style.color = 'white'
+          }
+        }
+      })
+    }
+  }
+
+  const handleReloadClick = id => {
+    fetchDataReload(id)
+  }
+
   return (
     <>
       <Grid container spacing={6.5}>
@@ -1028,26 +1075,15 @@ const Add = ({ apiData }) => {
                         <TableCell sx={{ padding: '16px' }}>{assetType.location}</TableCell>
                         <TableCell sx={{ padding: '16px', textAlign: 'center' }}>
                           {assetType.status && assetType.status.name ? (
-                            <div
-                              style={{
-                                backgroundColor:
-                                  assetType.status.name === 'connected'
-                                    ? '#9af7d0'
-                                    : assetType.status.name === 'disconnected'
-                                    ? '#95ef85'
-                                    : 'orange',
-                                borderRadius: '10px',
-                                padding: '5px 10px',
-                                width: '70%',
-                                display: 'inline-block',
-                                color: '#5e9154'
-                              }}
-                            >
-                              {assetType.status.name === 'connected'
-                                ? 'Đã kết nối'
-                                : assetType.status.name === 'disconnected'
-                                ? 'Mất kết nối'
-                                : assetType.status.name}
+                            <div>
+                              <CustomChip
+                                rounded
+                                size='small'
+                                skin='light'
+                                sx={{ lineHeight: 1 }}
+                                label={assetType.status.name === 'disconnected' ? 'Mất kết lỗi' : 'Đã kết lỗi'}
+                                color={assetType.status.name === 'disconnected' ? 'primary' : 'success'}
+                              />
                             </div>
                           ) : (
                             assetType.status.name
@@ -1055,6 +1091,9 @@ const Add = ({ apiData }) => {
                         </TableCell>
 
                         <TableCell sx={{ padding: '16px' }}>
+                          <IconButton onClick={() => handleReloadClick(assetType.id)}>
+                            <Icon icon='tabler:reload' />
+                          </IconButton>
                           <IconButton onClick={() => handleOpenLiveView(assetType)}>
                             <Icon icon='tabler:video' />
                           </IconButton>
