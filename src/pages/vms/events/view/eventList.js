76c6ce998e644bbc4af7ca5329f3927d46539b2a
Cường Nguyễn Nam <namcuongn1407@gmail.com>
2024-08-08 00:13:29 +0700

    wss for event

--- src/pages/vms/events/view/eventList.js
+++ src/pages/vms/events/view/eventList.js
@@ -35,7 +35,7 @@ const initValueFilter = {
   page: 1
 }
 
-const EventList = ({ eventData }) => {
+const EventList = ({  }) => {
   const [keyword, setKeyword] = useState('')
   const [valueFilter, setValueFilter] = useState(initValueFilter)
   const [loading, setLoading] = useState(false)
@@ -53,8 +53,99 @@ const EventList = ({ eventData }) => {
   const [pageSize, setPageSize] = useState(25)
   const pageSizeOptions = [25, 50, 100]
   const [anchorEl, setAnchorEl] = useState(null)
-
+  const [rtcPeerConnection, setRtcPeerConnection] = useState(null)
+  const [websocket, setWebsocket] = useState(null)
+  const [eventData, setEventData] = useState(null)
+  const defaultCameraID = '0eb23593-a9b1-4278-9fb1-4d18f30ed6ff'
   const token = localStorage.getItem(authConfig.storageTokenKeyName)
+
+  const config = {
+    bundlePolicy: 'max-bundle',
+    iceServers: [
+      {
+        urls: 'stun:dev-ivis-camera-api.basesystem.one:3478'
+      },
+      {
+        urls: 'turn:dev-ivis-camera-api.basesystem.one:3478',
+        username: 'demo',
+        credential: 'demo'
+      }
+    ]
+  }
+
+  useEffect(() => {
+    // create WebSocket connection
+
+    const ws = new WebSocket(
+      `wss://sbs.basesystem.one/ivis/vms/api/v0/websocket/topic/list_ai_event/be571c00-41cf-4878-a1de-b782625da62a`
+    )
+
+    setWebsocket(ws)
+
+    // create RTCPeerConnection
+
+    const pc = new RTCPeerConnection(config)
+    setRtcPeerConnection(pc)
+
+    // listen for remote tracks and add them to remote stream
+
+    pc.ontrack = event => {
+      const stream = event.streams[0]
+      if (!remoteVideoRef.current?.srcObject || remoteVideoRef.current?.srcObject.id !== stream.id) {
+        setRemoteStream(stream)
+        remoteVideoRef.current.srcObject = stream
+      }
+    }
+
+    // close WebSocket and RTCPeerConnection on component unmount
+
+    return () => {
+      if (websocket) {
+        websocket.close()
+      }
+      if (rtcPeerConnection) {
+        rtcPeerConnection.close()
+      }
+    }
+  }, [])
+
+  const handleMessage = async event => {
+    const message = JSON.parse(event.data)
+    const newMessage = JSON.parse(message?.data)
+    setEventData(newMessage)
+  }
+
+  useEffect(() => {
+    if (websocket) {
+      websocket.addEventListener('open', () => {
+        websocket.send(
+          JSON.stringify({
+            id: defaultCameraID,
+            type: 'request'
+          })
+        )
+      })
+      websocket.addEventListener('message', handleMessage)
+
+      websocket.addEventListener('error', error => {
+        console.error('WebSocket error:', error)
+      })
+
+      websocket.addEventListener('close', handleClose)
+    }
+  }, [websocket])
+
+  const handleClose = async event => {
+    if (websocket) {
+      websocket.close()
+    }
+  }
+
+  useEffect(() => {
+    if (rtcPeerConnection) {
+      rtcPeerConnection.addEventListener('connectionstatechange', () => {})
+    }
+  }, [rtcPeerConnection])
 
   const config1 = {
     headers: {
